cf6ed6c173f49d30e2f896a2e4e58d0b56f6e83c
Carlos Moreno <mysql.ducor@gmail.com>
2024-03-07 18:22:36 -0500

    se agrego publicaciones_rappi.routes

--- src/routes/publicaciones_rappi.routes.js
+++ src/routes/publicaciones_rappi.routes.js
@@ -3,9 +3,9 @@ import { pool } from '../../conection.js';
 
 const router = Router();
 
-router.get('/publicaciones_ml', async(req, res) =>{
+router.get('/publicaciones_rappi', async(req, res) => {
     try {
-        const {rows} = await pool.query("SELECT publicaciones_ml.*, publicaciones.nombre FROM publicaciones_ml INNER JOIN publicaciones ON publicaciones_ml.id = publicaciones.id");
+        const {rows} = await pool.query("SELECT * FROM publicaciones_rappi");
         if(rows.length === 0) return res.status(200).json({status: 204, mensaje: "No se encontro ningun publicaciones"})
 
         res.status(200).json({status: 200, data: rows})
@@ -14,9 +14,22 @@ router.get('/publicaciones_ml', async(req, res) =>{
     }
 });
 
-router.get('/publicaciones_ml/:id', async(req, res) =>{
+router.get('/publicacion_rappi/:id', async (req, res) =>{
     try {
-        const {rows} = await pool.query("SELECT publicaciones_ml.*, publicaciones.nombre FROM publicaciones_ml INNER JOIN publicaciones ON publicaciones_ml.id = publicaciones.id WHERE id = $1", [req.params.id]);
+        const {rows} = await pool.query("SELECT * FROM publicaciones_rappi WHERE id = $1", [req.params.id]);
+        if(rows.length === 0) return res.status(200).json({status: 204, mensaje: "No se encontro ningun publicaciones"})
+
+        res.status(200).json({status: 200, data: rows})
+    } catch (error) {
+        res.status(400).json({status: 400, mensaje: error});  
+    }
+})
+
+router.post('/publicacion_rappi', async(req, res) => {
+    const {id, producto_id, nombre } = req.body;
+
+    try {
+        const {rows} = await pool.query("INSERT INTO publicaciones_rappi(id,producto_id,nombre) VALUES ($1,$2,$3) RETURNING *", [id, producto_id, nombre]);
         if(rows.length === 0) return res.status(200).json({status: 204, mensaje: "No se encontro ningun publicaciones"})
 
         res.status(200).json({status: 200, data: rows});
@@ -25,19 +38,7 @@ router.get('/publicaciones_ml/:id', async(req, res) =>{
     }
 });
 
-router.post('/publicacion_ml', async(req,res) =>{
-    const {id, mco, variante} = req.body;
-    try {
-        const {rows} = await pool.query("INSERT INTO publicaciones_ml (id, mco, variante) VALUES ($1,$2,$3)", [id, mco, variante]);
-        if(rows.length === 0) return res.status(200).json({status: 204, mensaje: "No se encontro ningun publicaciones"})
-
-        res.status(200).json({status: 200, data: rows});
-    } catch (error) {
-        res.status(400).json({status: 400, mensaje: error});  
-    }
-});
-
-router.post('/publicaciones_ml', async(req,res) =>{
+router.post('/publicaciones_rappi', async(req,res) => {
     const {publicaciones} = req.body;
     
     if(!publicaciones || publicaciones.length === 0) return res.status(400).json({status: 400, mensaje: "No se envio los datos de los publicaciones"});
@@ -46,7 +47,7 @@ router.post('/publicaciones_ml', async(req,res) =>{
     let errores = [];
 
     for(var i = 0; i < Math.ceil(publicaciones.length/30); i++){
-        let query = "INSERT INTO publicaciones_ml (id,mco,variante) VALUES ";
+        let query = "INSERT INTO publicaciones_rappi (id,producto_id,nombre) VALUES ";
         var ronda = publicaciones.slice(i*30, ((i*30)+30));
 
         ronda.forEach((publicaciones, index) =>{
@@ -54,7 +55,7 @@ router.post('/publicaciones_ml', async(req,res) =>{
                 query += ",";
             } 
 
-            query+= "('"+publicaciones.id+"',"+publicaciones.mco+","+publicaciones.variante+")";
+            query+= "('"+publicaciones.id+"',"+publicaciones.producto_id+","+publicaciones.nombre+")";
         });
 
         try {
@@ -71,34 +72,6 @@ router.post('/publicaciones_ml', async(req,res) =>{
     if(errores.length === 0) return res.status(201).json({status: 201, confirmacion: "Se crearon todos las publicaciones", data: creados});
 
     res.status(200).json({status: 200, mensaje: "se crearon algunas publicaciones", data: creados, error: errores});
-});
-
-router.put('/publicacion_ml/:id', async(req, res) =>{
-    const {mco, variante, mco_anterior} = req.body;
-
-    let query = "";
-    let params = [req.params.id, mco_anterior];
-
-
-    if(mco && variante){
-        query = "UPDATE productos SET mco = $3, variante = $4 WHERE id = $1 AND mco = $2"
-    } else if(mco){
-        query = "UPDATE productos SET mco = $3 WHERE id = $1 AND mco = $2";
-        params.push(mco);
-    } else if(variante){
-        query = "UPDATE productos SET variante = $3 WHERE id = $1 AND mco = $2";
-        params.push(variante);
-    }
-
-    try {
-        const {rows} = await pool.query(query+" RETURNING *", params);
-
-        if(rows.length === 0) return res.status(200).json({status: 204, mensaje: "no se actualizo ninguna fila"});
-
-        res.status(200).json({status: 200, confirmacion: "Se actualizo la publicacion", data: rows[0]})
-    } catch (error) {
-        res.status(400).json({status: 400, mensaje: error})
-    }
 });
 
 export default router;
