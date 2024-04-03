43120bf59041af92aa82fd2f293ac3878946399a
Carlos Moreno <mysql.ducor@gmail.com>
2024-04-03 18:02:42 -0500

    mongo get

--- src/routes/pruebas.routes.js
+++ src/routes/pruebas.routes.js
@@ -4,7 +4,7 @@ import registroSchema from '../models/registro.js'
 const router = Router();
 
 router.get('/registrar', (req, res) => {
-
+    registroSchema.find().then((data) => res.json(data)).catch((error) => res.json({ menjase: error}));
 });
 
 router.post('/registrar', (req, res) =>{
