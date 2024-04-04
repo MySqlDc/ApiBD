23ccdeee24a0b38d58f99fd3877d862f0160569a
Carlos Moreno <mysql.ducor@gmail.com>
2024-04-04 10:48:23 -0500

    agregacion de registro

--- src/routes/pruebas.routes.js
+++ src/routes/pruebas.routes.js
@@ -3,12 +3,12 @@ import registro from '../models/registro.js'
 
 const router = Router();
 
-router.get('/registrar', (req, res) => {
-    registro.find().then((data) => res.json(data)).catch((error) => res.json({ menjase: error}));
+router.get('/registrar', async (req, res) => {
+    registro.find().then((data) => res.json(data)).catch((error) => res.json({ mensaje: error}));
 });
 
-router.post('/registrar', (req, res) =>{
-    const user = registro(req.body);
+router.post('/registrar', async (req, res) =>{
+    const user = new registro(req.body);
     user.save().then((data) => res.json(data)).catch((error) => res.json({ mensaje: error}));
 });
 
