6b1e4156e4fff171e665f1b70a2b7e8b5c76d95c
Carlos Moreno <mysql.ducor@gmail.com>
2024-04-05 12:06:01 -0500

    endpoint empaque en informes

--- src/routes/informes.routes.js
+++ src/routes/informes.routes.js
@@ -119,4 +119,46 @@ router.get('/comprasMarcas', async (req, res) => {
         res.status(400).json({status: 400, mensaje: error});
     }
 });
+
+router.get('/empaque', async (req, res) => {
+    const { fechas, fecha } = req.query;
+
+    let query = "SELECT p.nombre, COALESCE(packing_count, 0) AS packing_cantidad, COALESCE(picking_count, 0) AS picking_cantidad FROM personas p LEFT JOIN (SELECT usuario_picking, COUNT(*) AS picking_count FROM facturas_salidas WHERE picking IS NOT NULL ";
+
+    if(fechas){
+        let rango = fechas.split("/");
+        if(!validarFecha(rango[0])) return res.status(400).json({status: 400, mensaje: "la fecha 1 esta en un formato erroneo"});
+        if(!validarFecha(rango[1])) return res.status(400).json({status: 400, mensaje: "la fecha 2 esta en un formato erroneo"});
+
+        query += "AND fecha BETWEEN '"+rango[0]+"' AND '"+rango[1]+"' ";
+    } else if(fecha){
+        if(!validarFecha(fecha)) return res.status(400).json({status: 400, mensaje: "la fecha esta en un formato erroneo"});
+        query += "AND fecha = '"+fecha+"' ";
+    }
+
+    query += "GROUP BY usuario_picking) AS picking_counts ON p.id = picking_counts.usuario_picking LEFT JOIN (SELECT usuario_packing, COUNT(*) AS packing_count FROM facturas_salidas WHERE packing IS NOT NULL ";
+
+    if(fechas){
+        let rango = fechas.split("/");
+        if(!validarFecha(rango[0])) return res.status(400).json({status: 400, mensaje: "la fecha 1 esta en un formato erroneo"});
+        if(!validarFecha(rango[1])) return res.status(400).json({status: 400, mensaje: "la fecha 2 esta en un formato erroneo"});
+
+        query += "AND fecha BETWEEN '"+rango[0]+"' AND '"+rango[1]+"' ";
+    } else if(fecha){
+        if(!validarFecha(fecha)) return res.status(400).json({status: 400, mensaje: "la fecha esta en un formato erroneo"});
+        query += "AND fecha = '"+fecha+"' ";
+    }
+
+    query += "GROUP BY usuario_packing) AS packing_counts ON p.id = packing_counts.usuario_packing";
+
+    try {
+        const {rows} = await pool.query(query);
+
+        if(rows.length == 0) return res.status(200).json({status: 200, mensaje: "no se ha encontrado ningun dato coincidente"})
+
+        res.status(200).json({status: 200, data: rows})
+    } catch (error) {
+        res.status(400).json({status: 400, mensaje: error});
+    }
+});
 export default router;
