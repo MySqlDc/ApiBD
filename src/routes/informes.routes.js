93b6c39fc4fc5ed58c3474aa423d0cb77f0f2a88
Carlos Moreno <mysql.ducor@gmail.com>
2024-04-05 12:47:48 -0500

    agregar plataformas a las factura

--- src/routes/informes.routes.js
+++ src/routes/informes.routes.js
@@ -161,4 +161,34 @@ router.get('/empaque', async (req, res) => {
         res.status(400).json({status: 400, mensaje: error});
     }
 });
+
+router.get('/ventasPlataformas', async(req, res) => {
+    const { fechas, fecha } = req.query;
+
+    let query = "SELECT plataformas.nombre, COUNT(*) AS cantidad FROM facturas_salidas JOIN plataformas ON plataformas.id = facturas_salidas.plataforma "
+
+    if(fechas){
+        let rango = fechas.split("/");
+        query += "WHERE facturas_salidas.fecha BETWEEN ";
+        if(!validarFecha(rango[0])) return res.status(400).json({status: 400, mensaje: "la fecha 1 esta en un formato erroneo"});
+        query += "'"+rango[0]+"'";
+        if(!validarFecha(rango[1])) return res.status(400).json({status: 400, mensaje: "la fecha 2 esta en un formato erroneo"});
+        query += " AND '"+rango[1]+"'";
+    } else if(fecha) {
+        if(!validarFecha(fecha)) return res.status(400).json({status: 400, mensaje: "la fecha esta en un formato erroneo"});
+        query += "WHERE facturas_salidas.fecha = '"+fecha+"' ";
+    }
+
+    query += "GROUP BY plataformas.nombre ORDER BY cantidad DESC"
+    try {
+        const {rows} = await pool.query(query);
+
+        if(rows.length == 0) return res.status(200).json({status: 200, mensaje: "no se ha encontrado ningun dato coincidente"})
+
+        res.status(200).json({status: 200, data: rows})
+    } catch (error) {
+        res.status(400).json({status: 400, mensaje: error});
+    }
+});
+
 export default router;
