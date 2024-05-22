9c6411378e0572823615319ca396d558559a74c8
Carlos Moreno <mysql.ducor@gmail.com>
2024-05-22 12:51:11 -0500

    cambio actualizacion rappi V1

--- src/v1/services/api_rappi.js
+++ src/v1/services/api_rappi.js
@@ -77,8 +77,7 @@ export const actualizacion = async () => {
             let producto = {};
             if(datos.precio_venta > datos.precio_rappi){
                 producto = {
-                    id: datos.publicacion_id.toString(),
-                    ean: datos.producto_id.toString(),
+                    id: datos.producto_id.toString(),
                     store_id: STORE_ID_RAPPI,
                     name: datos.nombre,
                     trademark: datos.marca,
@@ -90,8 +89,7 @@ export const actualizacion = async () => {
                 }
             } else {
                 producto = {
-                    id: datos.publicacion_id.toString(),
-                    ean: datos.producto_id.toString(),
+                    id: datos.producto_id.toString(),
                     store_id: STORE_ID_RAPPI,
                     name: datos.nombre,
                     trademark: datos.marca,
