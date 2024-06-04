e7aa976c9380575f680e0165c70911e658d3fdbd
Carlos Moreno <mysql.ducor@gmail.com>
2024-06-04 14:50:29 -0500

    manejo errores middleware y 5 dias despues admite finales de mes

--- src/v2/validators/facturaValidator.js
+++ src/v2/validators/facturaValidator.js
@@ -1,5 +1,3 @@
 export const esDespues = (fecha1, fecha2) => {
-    return fecha1.getFullYear() === fecha2.getFullYear() &&
-        fecha1.getMonth() === fecha2.getMonth() &&
-        fecha1.getDate() <= fecha2.getDate();
+    return fecha1.getTime() <= fecha2.getTime()
 }
